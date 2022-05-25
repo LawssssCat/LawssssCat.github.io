2fa23c72d966b2a90a6466831e47eb8912e8d3ad
LawssssCat <1191693505@qq.com>
2022-05-25 12:38:52 +0800

    fix problems in calling code methods

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,5 @@
+---
+---
 // add permalink on headings
 // document.querySelectorAll('h2, h3, h4, h5, h5').forEach((heading) => {
 //     const link = document.createElement('a');
@@ -5,3 +7,33 @@
 //     link.href = `#${heading.id}`;
 //     heading.appendChild(link);
 // });
+
+(function (cnjs, methods) {
+
+// Event handling
+cnjs.onReady = function(ready) {
+  // in case the document is already rendered
+  if (document.readyState!='loading') ready();
+  // modern browsers
+  else if (document.addEventListener) document.addEventListener('DOMContentLoaded', ready);
+  // IE <= 8
+  else document.attachEvent('onreadystatechange', function(){
+      if (document.readyState=='complete') ready();
+  });
+}
+
+// set modules
+
+cnjs.setMethods = function(methods) {
+  cnjs.methods = methods;
+}
+
+// Document ready
+
+cnjs.onReady(function(){
+  cnjs.setMethods(methods);
+});
+
+})(window.cnjs = window.cnjs || {}, {
+  url: require('./common-url'),
+});
