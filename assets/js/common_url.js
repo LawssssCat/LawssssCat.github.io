fd58fd349b540453e6043db26d33f32652935eec
LawssssCat <1191693505@qq.com>
2022-05-25 18:55:49 +0800

    :exclamation: refactored search code

---
+++ assets/js/common_url.js
@@ -0,0 +1,64 @@
+//获取QueryString的数组
+function getQueryString(){
+  var result = location.search.match(new RegExp("[\?\&][^\?\&]+=[^\?\&]+","g"));
+  for(var i = 0; i < result.length; i++){
+    result[i] = result[i].substring(1);
+  }
+  return result;
+}
+//根据QueryString参数名称获取值
+function getQueryStringByName(name) {
+  var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
+  if (result == null || result.length < 1) {
+    return "";
+  }
+  return result[1];
+}
+//根据QueryString参数索引获取值
+function getQueryStringByIndex(index) {
+  if (index == null) {
+    return "";
+  }
+  var queryStringList = getQueryString();
+  if (index >= queryStringList.length) {
+    return "";
+  }
+  var result = queryStringList[index];
+  var startIndex = result.indexOf("=") + 1;
+  result = result.substring(startIndex);
+  return result;
+}
+
+// e.g.
+// http://www.cnblogs.com/page.aspx?id=6&c=7&d=8#asfasdf
+// replace C=7 to c=9
+var regularUrl = function (url,key,value) {
+    var fragPos = url.lastIndexOf("#");
+    var fragment="";
+    if(fragPos > -1) {
+      fragment = url.substring(fragPos);
+      url = url.substring(0,fragPos);
+    }
+    var querystart = url.indexOf("?");
+    if(querystart < 0) {
+      url += "?"+key+"="+value;
+    } else if (querystart==url.length-1) {
+      url += key+"="+value;
+    } else {
+        var Re = new RegExp(key+"=[^\\s&#]*","gi");
+        if (Re.test(url))
+          url=url.replace(Re,key+"="+value);
+        else
+          url += "&"+key+"="+value;
+    }
+    return url+fragment;
+}
+
+var appendQueryString = function(key, value) {
+  var reglaredUrl = regularUrl(location.href, key, value);
+  history.replaceState(null, null, reglaredUrl);
+}
+
+export default {
+  regularUrl,appendQueryString,getQueryStringByName
+}
--- assets/js/modules/post_table.js
+++ assets/js/modules/post_table.js
@@ -1,112 +1,115 @@
-(function(utils) {
-  function filter(query) {
-    var queryArray = query.toLowerCase().trim().split(/ *(\+|\*)/);
-    var postPattern = queryArray[0];
-    var categoryPatterns = [];
-    var mode;
-    queryArray.splice(1).forEach((item) => {
-      switch(item) {
-        case '+': mode = '+'; break;
-        case '*': mode = '*'; break;
-        default:
-          switch (mode) {
-              case '+': categoryPatterns.push(item);break;
-              default:console.log('error');break;
-          }
-          mode = '';
-          break;
-      }
-    });
-  
-    // filter rows
-    var noResults = true;
-    document.querySelectorAll('#post-table tbody tr').forEach(function (row) {
-        var show = true;
-  
-        var postName = row.getElementsByClassName('post-title')[0].innerHTML.toLowerCase();
-        if (postName.indexOf(postPattern) == -1) {
-            show = false;
+import common_url from '../common_url.js';
+
+var searchBox = document.getElementById('post-search');
+document.querySelectorAll("category-list category-item")
+var searchPatternKey = 'search_text';
+
+function filter(query) {
+  var queryArray = query.toLowerCase().trim().split(/ *(\+|\*)/);
+  var postPattern = queryArray[0];
+  var categoryPatterns = [];
+  var mode;
+  queryArray.splice(1).forEach((item) => {
+    switch(item) {
+      case '+': mode = '+'; break;
+      case '*': mode = '*'; break;
+      default:
+        switch (mode) {
+            case '+': categoryPatterns.push(item);break;
+            default:throw Error("Cannot find mode: "+mode);
         }
-  
-        if (show) { 
-            var categoryElems = Array.from(row.getElementsByClassName('category-list')[0].children);
-            categoryPatterns.forEach((pattern) => {
-                // skip empty filters
-                if (!pattern) {
-                    return;
-                }
-                // check against the pattern
-                var match = false;
-                categoryElems.forEach((item) => {
-                    if (item.innerText.trim().toLowerCase().startsWith(pattern.trim().toLowerCase())) {
-                        item.classList.add('match');
-                        match = true;
-                    } else {
-                        item.classList.remove('match');
-                    }
-                });
-                // pattern against
-                if(!match) {
-                  show = false;
-                }
-            });
-        }
-  
-        if (show) {
-            row.style.display = '';
-            noResults = false;
-        } else {
-            row.style.display = 'none';
-        }
-    });
-  
-    // update the search message visibility
-    var searchMessage = document.getElementById('search-message-no-result');
-    searchMessage.style.display = noResults ? 'table-cell' : 'none';
-  }
-  
-  function applyFilter() {
-    // filter on load according to the URL
-    var searchBox = document.getElementById('post-search');
-    var query = decodeURIComponent(location.hash.slice(1).split('?')[0]);
-    filter(query);
-    if (query) {
-        searchBox.value = query;
+        mode = '';
+        break;
     }
-  }
+  });
   
-  function setup() {
-    // handle user input
-    var searchBox = document.getElementById('post-search');
-    searchBox.addEventListener('input', function () {
-        var query = searchBox.value;
-        history.replaceState(null, null, encodeURI('#' + query));
-        applyFilter();
-    });
-  
-    // handle shortcuts
-    addEventListener('keydown', function (event) {
-        // focus search box on valid keydown
-        if (event.key.toLowerCase().match(/^[+a-z]$/) &&
-            !(event.ctrlKey || event.altKey || event.metaKey)) {
-            searchBox.focus();
-            searchBox.parentElement.scrollIntoView();
-        }
-        // clear filter on escape
-        else if (event.key === 'Escape') {
-            location.hash = searchBox.value = '';
-            searchBox.focus();
-            searchBox.parentElement.scrollIntoView();
-        }
-    });
-  
-    // handle URL changes
-    window.onhashchange = applyFilter;
-  
-    // trigger filter on page load
-    applyFilter();
-  }
-  
-  setup();
-})({
-});
+  // filter rows
+  var noResults = true;
+  document.querySelectorAll('#post-table tbody tr').forEach(function (row) {
+      var show = true;
+
+      var postName = row.getElementsByClassName('post-title')[0].innerHTML.toLowerCase();
+      if (postName.indexOf(postPattern) == -1) {
+          show = false;
+      }
+
+      if (show) { 
+          var categoryElems = Array.from(row.getElementsByClassName('category-list')[0].children);
+          categoryPatterns.forEach((pattern) => {
+              // skip empty filters
+              if (!pattern) {
+                  return;
+              }
+              // check against the pattern
+              var match = false;
+              categoryElems.forEach((item) => {
+                  if (item.innerText.trim().toLowerCase().startsWith(pattern.trim().toLowerCase())) {
+                      item.classList.add('match');
+                      match = true;
+                  } else {
+                      item.classList.remove('match');
+                  }
+              });
+              // pattern against
+              if(!match) {
+                show = false;
+              }
+          });
+      }
+
+      if (show) {
+          row.style.display = '';
+          noResults = false;
+      } else {
+          row.style.display = 'none';
+      }
+  });
+
+  // update the search message visibility
+  var searchMessage = document.getElementById('search-message-no-result');
+  searchMessage.style.display = noResults ? 'table-cell' : 'none';
+}
+
+function applyFilter(query) {
+  common_url.appendQueryString(searchPatternKey, encodeURIComponent(query));
+  filter(query);
+}
+
+function setup() {
+  // handle user click 
+  document.querySelectorAll(".category-list .category-item").forEach((item) => {
+    item.addEventListener('click', () => {
+      var label = item.getAttribute('data-label');
+      searchBox.value += label;
+      applyFilter(searchBox.value);
+    })
+  });
+
+  // handle user input
+  searchBox.addEventListener('input', function () {
+    applyFilter(searchBox.value);
+  });
+
+  // handle shortcuts
+  addEventListener('keydown', function (event) {
+      // focus search box on valid keydown
+      if (event.key.toLowerCase().match(/^[+a-z]$/) &&
+          !(event.ctrlKey || event.altKey || event.metaKey)) {
+          searchBox.focus();
+          searchBox.parentElement.scrollIntoView();
+      }
+      // clear filter on escape
+      else if (event.key === 'Escape') {
+          location.hash = searchBox.value = '';
+          searchBox.focus();
+          searchBox.parentElement.scrollIntoView();
+      }
+  });
+
+  // trigger filter on page load
+  var pattern = common_url.getQueryStringByName(searchPatternKey);
+  pattern = decodeURIComponent(pattern);
+  applyFilter(pattern);
+}
+
+setup();
