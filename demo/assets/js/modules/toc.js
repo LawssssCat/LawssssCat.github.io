75c1dde626f3b10bab9364d92d230dad323867af
LawssssCat <1191693505@qq.com>
2022-05-29 14:07:02 +0000

    Deploying to gh-pages from @ LawssssCat/LawssssCat.github.io@89701242e4967fcaaf9f4c692e98ee51889db648 🚀

--- demo/assets/js/modules/toc.js
+++
@@ -1,7 +0,0 @@
-// add permalink on headings
-document.querySelectorAll('h2, h3, h4, h5, h5').forEach((heading) => {
-  const link = document.createElement('a');
-  link.className = 'permalink';
-  link.href = `#${heading.id}`;
-  heading.appendChild(link);
-});
